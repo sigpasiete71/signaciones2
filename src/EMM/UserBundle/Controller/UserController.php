fbb3ec0f7aaf239f4c50f42cc9e5005321c74e1d
sigpasiete71 <sigpasiete7@gmail.com>
2016-02-26 02:52:08 +0000

    mustra form ingresar

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace EMM\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use EMM\UserBundle\Entity\User;
+use EMM\UserBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -14,5 +16,63 @@ class UserController extends Controller
         return $this->render('EMMUserBundle:User:index.html.twig',array('users'=>$users));
         
     }
+   public function addAction()
+    {
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
+    }
+    
+    private function createCreateForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array(
+                'action' => $this->generateUrl('emm_user_create'),
+                'method' => 'POST'
+            ));
+        
+        return $form;
+    }
+    
+     public function createAction(Request $request)
+    {   
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        $form->handleRequest($request);
+        
+        if($form->isValid())
+        {
+            $password = $form->get('password')->getData();
+            
+            $passwordConstraint = new Assert\NotBlank();
+            $errorList = $this->get('validator')->validate($password, $passwordConstraint);
+            
+            if(count($errorList) == 0)
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                
+                $user->setPassword($encoded);
+                
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+                
+                $successMessage = $this->get('translator')->trans('The user has been created.');
+                $this->addFlash('mensaje', $successMessage);
+                
+                return $this->redirectToRoute('emm_user_index');                
+            }
+            else
+            {
+                $errorMessage = new FormError($errorList[0]->getMessage());
+                $form->get('password')->addError($errorMessage);
+            }
+
+        }
+        
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
+    }
+    
     
 }
