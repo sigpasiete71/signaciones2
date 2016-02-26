aa86c1727884264429712e9d8564e1e4452638a2
sigpasiete71 <sigpasiete7@gmail.com>
2016-02-26 03:13:14 +0000

    ya inserta un user

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace EMM\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
 use EMM\UserBundle\Entity\User;
 use EMM\UserBundle\Form\UserType;
 
@@ -42,33 +44,12 @@ class UserController extends Controller
         
         if($form->isValid())
         {
-            $password = $form->get('password')->getData();
+             
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
             
-            $passwordConstraint = new Assert\NotBlank();
-            $errorList = $this->get('validator')->validate($password, $passwordConstraint);
-            
-            if(count($errorList) == 0)
-            {
-                $encoder = $this->container->get('security.password_encoder');
-                $encoded = $encoder->encodePassword($user, $password);
-                
-                $user->setPassword($encoded);
-                
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($user);
-                $em->flush();
-                
-                $successMessage = $this->get('translator')->trans('The user has been created.');
-                $this->addFlash('mensaje', $successMessage);
-                
-                return $this->redirectToRoute('emm_user_index');                
-            }
-            else
-            {
-                $errorMessage = new FormError($errorList[0]->getMessage());
-                $form->get('password')->addError($errorMessage);
-            }
-
+           return $this->redirecttoRoute('emm_user_index');
         }
         
         return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
--- src/EMM/UserBundle/Entity/User.php
+++ src/EMM/UserBundle/Entity/User.php
@@ -3,14 +3,20 @@
 namespace EMM\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * User
  *
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="EMM\UserBundle\Entity\UserRepository")
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
+ * @ORM\HasLifecycleCallbacks()
  */
-class User
+class User implements UserInterface
 {
     /**
      * @var integer
@@ -24,7 +30,8 @@ class User
     /**
      * @var string
      *
-     * @ORM\Column(name="username", type="string", length=100)
+     * @ORM\Column(name="username", type="string", length=50)
+     * @Assert\NotBlank()
      */
     private $username;
 
@@ -32,6 +39,7 @@ class User
      * @var string
      *
      * @ORM\Column(name="first_name", type="string", length=100)
+     * @Assert\NotBlank()
      */
     private $firstName;
 
@@ -39,6 +47,7 @@ class User
      * @var string
      *
      * @ORM\Column(name="last_name", type="string", length=100)
+     * @Assert\NotBlank()
      */
     private $lastName;
 
@@ -46,20 +55,24 @@ class User
      * @var string
      *
      * @ORM\Column(name="email", type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="password", type="string", length=100100)
+     * @ORM\Column(name="password", type="string", length=255)
      */
     private $password;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="role", type="string", columnDefinition="ENUM('ROLE_ADMIN','ROLE_USER')")
+     * @ORM\Column(name="role", type="string", columnDefinition="ENUM('ROLE_ADMIN', 'ROLE_USER')", length=50)
+     * @Assert\NotBlank()
+     * @Assert\Choice(choices = {"ROLE_ADMIN", "ROLE_USER"})
      */
     private $role;
 
@@ -300,5 +313,37 @@ class User
     public function getUpdatedAt()
     {
         return $this->updatedAt;
+    }
+    
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedAtValue()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+    
+    public function getRoles()
+    {
+        
+    }
+    
+    public function getSalt()
+    {
+        
+    }
+    
+    public function eraseCredentials()
+    {
+        
     }
 }
