06274b074e2dd9706cfc8f8ac370263a217441fd
sigpasiete71 <sigpasiete7@gmail.com>
2016-03-03 15:16:00 +0000

    actualizar y ver

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     {
         $em=$this->getDoctrine()->getManager();
         
-        $dql="SELECT u FROM EMMUserBundle:User u";
+        $dql="SELECT u FROM EMMUserBundle:User u ORDER BY u.id DESC";
         $users=$em->createQuery($dql);    
         $paginator=$this->get('knp_paginator');
         $pagination=$paginator->paginate(
@@ -45,7 +45,7 @@ class UserController extends Controller
         return $form;
     }
     
-     public function createAction(Request $request)
+    public function createAction(Request $request)
     {   
         $user = new User();
         $form = $this->createCreateForm($user);
@@ -61,11 +61,64 @@ class UserController extends Controller
             $succesMessage=$this->get('translator')->trans('The user has been created.');
             $this->addFlash('mensaje',$succesMessage);
             
-           return $this->redirecttoRoute('emm_user_index');
+           return $this->redirectToRoute('emm_user_index');
         }
         
         return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
     }
+    public function editAction($id){
+        $em=$this->getDoctrine()->getManager();
+        $user=$em->getRepository('EMMUserBundle:User')->find($id);
+        if(!$user){
+            $messageExeption=$this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundExeption($messageExeption);
+        }
+        $form=$this->createEditForm($user);
+        return $this->render('EMMUserBundle:User:edit.html.twig',array('user' => $user, 'form' => $form->createView()));
+        
+    }
+    private function createEditForm(User $entity){
+        $form= $this->createForm(new UserType(),$entity,array('action' => $this->generateUrl
+        ('emm_user_update',array('id' => $entity->getId())),'method'=> 'PUT' ));
+        return $form;
+        
+    }
     
+    public function updateAction($id,Request $request){
+        $em=$this->getDoctrine()->getManager();
+        $user=$em->getRepository('EMMUserBundle:User')->find($id);
+        if(!$user){
+            $messageExeption=$this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundExeption($messageExeption);
+        }
+        $form =$this->createEditForm($user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+             
+            
+            $em->flush();
+            $succesMessage=$this->get('translator')->trans('The user has been modified.');
+            $this->addFlash('mensaje',$succesMessage);
+            
+            return $this->redirectToRoute('emm_user_edit',array('id' => $user->getId()));
+            
+        }
+        return $this->render('EMMUserBundle:User:edit.html.twig',array('user' => $user, 'form' => $form->createView()));
+    }
+ 
+   public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+        $user = $repository->find($id);
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($messageException);
+        }
+        
+        //$deleteForm = $this->createCustomForm($user->getId(), 'DELETE', 'emm_user_delete');
+        
+        return $this->render('EMMUserBundle:User:view.html.twig', array('user' => $user));
+    }
     
 }
